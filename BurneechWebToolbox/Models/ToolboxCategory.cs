0adb093d75be6028bc4c20e3a73942f0cf29964c
burneech <81528115+burneech@users.noreply.github.com>
2021-10-28 21:32:45 +0200

    Created Razor components, added a JSON file with all the necessary site data, changed image names, fixed components showing in front of the top-row element.

---
+++ BurneechWebToolbox/Models/ToolboxCategory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BurneechWebToolbox.Models
+{
+    public class ToolboxCategory
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Href { get; set; }
+        public string Summary { get; set; }
+        public List<ToolboxCategoryPage> Pages { get; set; }
+    }
+}
---
+++ BurneechWebToolbox/Models/ToolboxCategoryPage.cs
@@ -0,0 +1,9 @@
+﻿namespace BurneechWebToolbox.Models
+{
+    public class ToolboxCategoryPage
+    {
+        public string Title { get; set; }
+        public string URL { get; set; }
+        public string Summary { get; set; }
+    }
+}
